bec53e6faaf52364ee550a84ed628e1e63414761
vedantchoubey098 <49265501+vedantchoubey098@users.noreply.github.com>
2021-03-14 15:28:56 +0530

    working on layout with material icons

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHrag1wN1DCNWMObsljGr4bF4yH0k21oQ",
+    authDomain: "whatsupp-web.firebaseapp.com",
+    projectId: "whatsupp-web",
+    storageBucket: "whatsupp-web.appspot.com",
+    messagingSenderId: "704776331346",
+    appId: "1:704776331346:web:39676af3f04785c6e5ff8b",
+    measurementId: "G-5K586KV4FZ"
+  };
