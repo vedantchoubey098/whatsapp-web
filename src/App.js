628387637fd277c18d145a4aa0a552013e4b6572
vedantchoubey098 <49265501+vedantchoubey098@users.noreply.github.com>
2021-03-14 22:16:05 +0530

    adding feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-
 import './App.css';
-import Sidebar from './components/Sidebar';
+import React, {useState} from 'react';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import Login from './Login';
+import { BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import {useStateValue} from './StateProvider';
+
 function App() {
+  const [{user}, dispatch] = useStateValue();
   return (
     <div className="app">
-    
-     <div className="app__body">
-      <Sidebar /> 
-     </div>
+        
+          <div className="app_body">
+            <Router>
+              <Sidebar/>
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat/>
+                </Route>
+                <Route path="/">
+                  <Chat/>
+                </Route>              
+              </Switch>            
+            </Router>
+          </div>
+       
+        
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,19 @@
+import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//GET Below Settings from Firebase > Project Overview > Settings > General > Your apps > Firebase SDK snippet > Config
 const firebaseConfig = {
-    apiKey: "AIzaSyBHrag1wN1DCNWMObsljGr4bF4yH0k21oQ",
-    authDomain: "whatsupp-web.firebaseapp.com",
-    projectId: "whatsupp-web",
-    storageBucket: "whatsupp-web.appspot.com",
-    messagingSenderId: "704776331346",
-    appId: "1:704776331346:web:39676af3f04785c6e5ff8b",
-    measurementId: "G-5K586KV4FZ"
-  };
+  apiKey: "AIzaSyBHrag1wN1DCNWMObsljGr4bF4yH0k21oQ",
+  authDomain: "whatsupp-web.firebaseapp.com",
+  projectId: "whatsupp-web",
+  storageBucket: "whatsupp-web.appspot.com",
+  messagingSenderId: "704776331346",
+  appId: "1:704776331346:web:39676af3f04785c6e5ff8b",
+  measurementId: "G-5K586KV4FZ"
+};
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore(); 
+  const auth = firebaseApp.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth,provider};
+  export default db;
